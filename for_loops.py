bbef3b25f622c53bd9892ab9e10346bd541426ce
ltoebbe <144365202+ltoebbe@users.noreply.github.com>
2023-11-01 09:02:18 -0400

    it is wednesday my dudes

--- for_loops.py
+++ for_loops.py
@@ -6,15 +6,29 @@ def count(first, last):
     the first number is larger than the second, the numbers should count down, rather than up.
     count(5, 10) returns "5 6 7 8 9 10 "
     :param first: The starting number
-    :param second: The final number. Must be included
+    :param last: The final number. Must be included
     :return: A string containing the numbers
     """
-    pass # make sure to delete this line when you write your own function
+
+# test = ""
+# for x in range(7):
+#     test += str(x)
+
+def count(first, last):
+    test = ""
+    for x in range(first, last):
+        test += str(x)
+    print(test)
+
+def countdown(last, first):
+    test = ""
+    for x in range(first, last):
+        test += str(x)
+    print(test)
 
 
 def main():
     print(count(0, 6))
-
-
+    print(countdown(6, 0))
 if __name__ == '__main__':
     main()

d2357f6243fe1d01cc9f013e46fe95bc221dab14
ltoebbe <144365202+ltoebbe@users.noreply.github.com>
2023-11-02 11:59:22 -0400

    it is wednesday my dudes

--- fibonacci.py
+++ fibonacci.py
@@ -1,6 +1,11 @@
-def fibonacci(x):
-    """
-    Ex. fibonacci(5) returns "1 1 2 3 5 "
-    :param number: The number of Fibonacci terms to return
-    :return: A string consisting of a number of terms of the Fibonacci sequence.
-    """
+
+"""
+Ex. fibonacci(5) returns "1 1 2 3 5 "
+:param number: The number of Fibonacci terms to return
+:return: A string consisting of a number of terms of the Fibonacci sequence.
+"""
+def fibonacci(number):
+    test = ""
+    for x in range(number):
+        test += str(x)
+    print(test)
--- for_loops.py
+++ for_loops.py
@@ -1,34 +1,28 @@
 
-def count(first, last):
-    """
-    This function will create a string of numbers separated by a space. The numbers will start with the
-    first number and end with the second. The second number SHOULD be included as part of the string. If
-    the first number is larger than the second, the numbers should count down, rather than up.
-    count(5, 10) returns "5 6 7 8 9 10 "
-    :param first: The starting number
-    :param last: The final number. Must be included
-    :return: A string containing the numbers
-    """
 
-# test = ""
-# for x in range(7):
-#     test += str(x)
-
-def count(first, last):
-    test = ""
-    for x in range(first, last):
-        test += str(x)
-    print(test)
-
-def countdown(last, first):
-    test = ""
-    for x in range(first, last):
-        test += str(x)
-    print(test)
+# def count(first, last):
+#     test = ""
+#     for x in range(first, last):
+#         test += str(x)
+#     print(test)
+#
+# def countdown(first, last, step):
+#     test2 = ""
+#     for x in range(first, last, step):
+#         test2 += str(x)
+#     print(test2)
+#
+#
+# def main():
+#     count(0, 6)
+#     countdown(6, 0, -1)
+# if __name__ == '__main__':
+#     main()
 
 
-def main():
-    print(count(0, 6))
-    print(countdown(6, 0))
-if __name__ == '__main__':
-    main()
+def mult_table(number):
+    for x in range(1, 12):
+        answers = number * int(x)
+        print(answers)
+
+mult_table(6)

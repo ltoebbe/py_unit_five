f2169cc4f904255c01933c2204b785385a84dc0a
ltoebbe <144365202+ltoebbe@users.noreply.github.com>
2023-11-03 14:17:52 -0400

    it is wednesday my dudes

---
+++ average.py
@@ -0,0 +1,13 @@
+# average = (value1 + value2 etc.)/number of values
+counter = 0
+value = int(input("enter value: "))
+
+# while value != "q":
+#     counter += 1
+#     value = int(input("enter value: "))
+#     total = value + suM
+#     value = total
+#     if value == 'q':
+#         break
+#         average = (int(total)) * counter
+# print(average)
--- fibonacci.py
+++ fibonacci.py
@@ -4,8 +4,11 @@ Ex. fibonacci(5) returns "1 1 2 3 5 "
 :param number: The number of Fibonacci terms to return
 :return: A string consisting of a number of terms of the Fibonacci sequence.
 """
+
+
 def fibonacci(number):
     test = ""
+    number = input("how many digits of the fibonacci sequence would you like to calculate? ")
     for x in range(number):
         test += str(x)
-    print(test)
+    print()

32748627a90ea6882fc38dcb08616f77a07c6eec
ltoebbe <144365202+ltoebbe@users.noreply.github.com>
2023-11-08 09:02:54 -0500

    it is wednesday my dudes

--- assignment_five.py
+++ assignment_five.py
@@ -17,20 +17,36 @@ def generate_number():
     return int(random_number)
 
 
-def guess():
-    input("guess a number between 1 and 100: ")
+def user_guess():
+    guess = input("guess a number between 1 and 100: ")
+    return guess
 
-print("let's play a game of hot and cold!"
-          "Try to guess a number between 1 and 100. I'll give you hints after each guess."
-          "'Warmer' means you're closer, 'colder' means you're farther away.")
+
+print("let's play a game of hot and cold!")
+print("Try to guess a number between 1 and 100. I'll give you hints after each guess.")
+print("'Warmer' means you're closer, 'colder' means you're farther away.")
 
 
 def main():
     random_number = generate_number()
     counter = 0
     while True:
-        guess()
+        guess = user_guess()
         counter = counter + 1
-        if int(guess()) == random_number:
+        if int(guess) == random_number:
             print("you got it!")
-        elif int(guess()) ==
+            break
+        elif 1 < (abs(int(guess)) - random_number) <= 5:
+            print("your blood is boiling and your hair is on fire")
+        elif 5 < (abs(int(guess)) - random_number) <= 25:
+            print("you have a pretty bad sunburn")
+        elif 25 < (abs(int(guess)) - random_number) <= 50:
+            print("you are the exact average temperature of a healthy human at rest")
+        elif 50 < (abs(int(guess)) - random_number) <= 75:
+            print("your teeth are chattering and you can't feel your toes")
+        elif 75 < (abs(int(guess)) - random_number) <= 100:
+            print("you have stage 3 hypothermia")
+    print("it took you", counter, "tries to guess the number.")
+
+
+main()

9656c66d20152ca3f2cd3a2ccff4be0617a48e78
ltoebbe <144365202+ltoebbe@users.noreply.github.com>
2023-11-12 19:25:43 -0500

    done

--- assignment_five.py
+++ assignment_five.py
@@ -1,23 +1,16 @@
 # Liam Toebbe
 # Nov. 6 2023
 
-# generate number between 1 and 100
-# prompt user for guess
-# print hint after each guess
-# if they get it, print win statement
-# repeat for 3 total games
-# report on regularity of success
-
 import random
 import math
 
 
-def generate_number():
+def generate_number():                                                                      # determines the number the user has to guess
     random_number = random.randint(1, 100)
     return int(random_number)
 
 
-def user_guess():
+def user_guess():                                                                           # asks user to input a guess between 1 and 100
     guess = input("guess a number between 1 and 100: ")
     return guess
 
@@ -32,23 +25,22 @@ def main():
     counter = 0
     while True:
         guess = user_guess()
-        absolute_value = abs(int(guess) - random_number)
-        counter = counter + 1
-        print(random_number)
-        if int(guess) == random_number:
+        absolute_value = abs(int(guess) - random_number)                                    # determines how far away the guess is from the answer
+        counter = counter + 1                                                               # keeps track of every try by adding 1 each loop
+        if int(guess) == random_number:                                                     # if the guess is correct, print a win statement
             print("you got it!")
-            break
-        elif 0 < absolute_value and absolute_value <= 5:
+            break                                                                           # if the guess is correct stop the loop
+        elif 0 < absolute_value and absolute_value <= 5:                                    # if the guess is within 5 of the answer, print appropriate hint
             print("your blood is boiling and your hair is on fire")
-        elif 5 < absolute_value and absolute_value <= 25:
+        elif 5 < absolute_value and absolute_value <= 25:                                   # if the guess is within 6-25 of the answer, print appropriate hint
             print("you have a pretty bad sunburn")
-        elif 25 < absolute_value and absolute_value <= 50:
+        elif 25 < absolute_value and absolute_value <= 50:                                  # if the guess is within 26-50 of the answer, print appropriate hint
             print("you are the exact average temperature of a healthy human at rest")
-        elif 50 < absolute_value and absolute_value <= 75:
+        elif 50 < absolute_value and absolute_value <= 75:                                  # if the guess is within 51-75 of the answer, print the appropriate hint
             print("your teeth are chattering and you can't feel your toes")
-        elif 75 < absolute_value and absolute_value <= 100:
+        elif 75 < absolute_value and absolute_value <= 100:                                 # if the guess is within 76-100 of the answer, print the appropriate hint
             print("you have stage 3 hypothermia")
-    print("it took you", counter, "tries to guess the number.")
+    print("it took you", counter, "tries to guess the number.")                             # once the answer is guessed, print how many guesses it took
 
 
 main()
